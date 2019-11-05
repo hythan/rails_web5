41a803fd505e048ace669972b2ab073af55d6294
Hythan <hythan97@gmail.com>
2019-11-05 20:45:07 -0300

    Genereted Users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :set_locale
+  before_action :autenticate_user!
 
   def set_locale
     session[:locale] = params[:locale] || session[:locale] || I18n.default_locale
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_05_215434) do
+ActiveRecord::Schema.define(version: 2019_11_05_234317) do
 
   create_table "contacts", force: :cascade do |t|
     t.string "name"
@@ -35,6 +35,18 @@ ActiveRecord::Schema.define(version: 2019_11_05_215434) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["contact_id"], name: "index_phones_on_contact_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
