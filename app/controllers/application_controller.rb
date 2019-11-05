d02343959523188f357bfad7b25a541a44ca6612
Hythan <hythan97@gmail.com>
2019-11-05 13:58:40 -0300

    New option menu lenguage select

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+  before_action :set_locale
+
+  def set_locale
+    session[:locale] = params[:locale] || session[:locale] || I18n.default_locale
+    I18n.locale = session[:locale]
+  end
 end
